7dcc97b68f0f6cb2aed36155d563d330e827558e
VishalVinayRam <vishalvinayram5432@gmail.com>
2024-02-07 14:25:34 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/home', (req, res) => {
+  res.send('GET request to the homepage')
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
